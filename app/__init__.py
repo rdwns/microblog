85369a2f2e950df705a62c042ceb18da53e59dd3
Ridwan <ridwan@bmsit.ac.in>
2020-04-16 17:31:32 +0530

    added error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import logging
+from logging.handlers import SMTPHandler
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -11,5 +13,34 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
+if not app.debug:
 
-from app import routes, models
+    mailserver = app.config['MAIL_SERVER']
+    mailport = app.config['MAIL_PORT']
+    mailusername = app.config['MAIL_USERNAME']
+    mailpassword = app.config['MAIL_PASSWORD']
+    to = app.config['ADMINS']
+    usetls = app.config['MAIL_USE_TLS']
+
+    if mailserver:
+        auth = None
+        if mailusername or mailpassword:
+            auth = (mailusername, mailpassword)
+
+        secure = None
+        if usetls:
+            secure = ()
+
+        mail_handler = SMTPHandler(
+            mailhost=(mailserver, mailport),
+            fromaddr='noreply@'+mailserver,
+            toaddrs=to,
+            subject='App Failure',
+            credentials=auth,
+            secure=secure
+            )
+        mail_handler.setLevel(logging.ERROR)
+
+        app.logger.addHandler(mail_handler)
+
+from app import routes, models, errors
