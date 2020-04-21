28b065bb41e49d5150b497e09efe93a89e55b5b9
Ridwan <ridwan@bmsit.ac.in>
2020-04-21 14:31:48 +0530

    added async behaviour for mail

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +20,11 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 if not app.debug:
 
